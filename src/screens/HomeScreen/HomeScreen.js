90ba20b6d2c3a3f93b2674a40e6893eefe2e1067
Ibrahim Kamal <ebrahiiim.kamal@gmail.com>
2021-07-17 15:34:57 +0200

    fetch different movies in Home page

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,15 +1,28 @@
+import requests from '../../api/requests';
+
 import Banner from '../../components/Banner/Banner';
 import Nav from '../../components/Nav/Nav';
+import Row from '../../components/Row/Row';
+
 import './HomeScreen.css';
 
 const HomeScreen = () => {
   return (
     <div className="homeScreen">
-      {/* Nav */}
       <Nav />
-      {/* Banner */}
       <Banner />
-      {/* Row */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorroryMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentariesMovies} />
     </div>
   );
 };
