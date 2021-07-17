5c34d0bb2d5895998ce7315d5a338e525f3449a2
Ibrahim Kamal <ebrahiiim.kamal@gmail.com>
2021-07-17 10:41:21 +0200

    setup firebase and start new project let's go

---
+++ src/firebase/index.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyADLxuz2doeBhDNqlje9LCuzBrpkEw_i00',
+  authDomain: 'react-netflix-75998.firebaseapp.com',
+  projectId: 'react-netflix-75998',
+  storageBucket: 'react-netflix-75998.appspot.com',
+  messagingSenderId: '230145547114',
+  appId: '1:230145547114:web:bff1d632054ed011f12313',
+};
