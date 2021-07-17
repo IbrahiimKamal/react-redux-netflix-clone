fd0d3bc8b3eabbd6d184de5bdf148c3fa3465f33
Ibrahim Kamal <ebrahiiim.kamal@gmail.com>
2021-07-17 19:08:11 +0200

    added authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import HomeScreen from './screens/HomeScreen/HomeScreen';
 import LoginScreen from './screens/LoginScreen/LoginScreen';
 
+import { auth } from './firebase';
+
 import './App.css';
 
 const App = () => {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // logged in
+        console.log(userAuth);
+      } else {
+        // logged out
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyADLxuz2doeBhDNqlje9LCuzBrpkEw_i00',
   authDomain: 'react-netflix-75998.firebaseapp.com',
@@ -6,3 +8,19 @@ const firebaseConfig = {
   messagingSenderId: '230145547114',
   appId: '1:230145547114:web:bff1d632054ed011f12313',
 };
+
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
